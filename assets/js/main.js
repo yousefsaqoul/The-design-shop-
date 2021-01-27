21f81bc2e4b654d6775ff5429881689208efbe43
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-01-27 01:29:06 +0100

    The-design-shop-

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+
+
+});
+
+
+function openMenu (){
+    let menu = document.getElementById("myLi");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+
+  
